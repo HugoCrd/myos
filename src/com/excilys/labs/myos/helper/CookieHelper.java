915eb210c14922f8d02efd1de27cd8efd93fc8a0
HugoCrd <hugo.cor@gmail.com>
2012-06-25 20:25:36 +0200

    Cookie setting is delegated to CookieHelper

--- src/com/excilys/labs/myos/helper/CookieHelper.java
+++ src/com/excilys/labs/myos/helper/CookieHelper.java
@@ -3,6 +3,8 @@ package com.excilys.labs.myos.helper;
 
 import java.util.Map;
 
+import org.vertx.java.core.http.HttpServerRequest;
+
 public class CookieHelper {
 
 	public static String getSessionid(Map<String, String> headers){
@@ -10,5 +12,9 @@ public class CookieHelper {
 			return headers.get("Cookie").replaceFirst("sessionid=", "").replaceFirst(";.*", "");
 		else
 			return null;
+	}
+	
+	public static void setSessionid(String sessionid, HttpServerRequest req){
+		req.response.headers().put("Set-Cookie", "sessionid="+sessionid+"; Expires=Tue, 15 Jan 2013 20:00:00 GMT; Path=/; Domain=localhost");
 	}
 }
